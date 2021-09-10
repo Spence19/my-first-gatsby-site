c71cacc61fefe7b7af50cc2b453cc2188a6d1533
Myles <spence.dow@hotmail.com>
2021-09-10 14:07:52 -0400

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/layout'
 
@@ -5,6 +6,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial</p>
+      <StaticImage
+        alt="Dinosaur"
+        src="https://static.wikia.nocookie.net/jurassicpark/images/4/4c/Stegosaurus-detail-header.png/revision/latest?cb=20141230042616"
+        />
     </Layout>
   )
 }
