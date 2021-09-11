606df656741275b34d79422d4f92a192542a1516
Myles <spence.dow@hotmail.com>
2021-09-11 14:32:42 -0400

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial</p>
+      <p>I'm making this by following the Gatsby Tutorial!</p>
       <StaticImage
         alt="Dinosaur"
-        src="https://static.wikia.nocookie.net/jurassicpark/images/4/4c/Stegosaurus-detail-header.png/revision/latest?cb=20141230042616"
+        src="../images/Stegosaurus-detail-header.webp"
         />
     </Layout>
   )
