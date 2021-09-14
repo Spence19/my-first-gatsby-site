854f9e28af2c56b5a421ccd99f6648efb0b210c3
Myles <spence.dow@hotmail.com>
2021-09-14 15:29:53 -0400

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
         path: `${__dirname}/blog`
       }
-    }
+    }    
   ],
 };
